2797c237b5a22ad202279d92ec6f73400c154c81
Vaceslav <vaceslavsergunin@gmail.com>
2022-09-04 08:34:36 +0600

    добавил строки

---
+++ Language/C#/lession-1/Exampels002_HeloUser/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите ваше имя"); // ReadLine - при помоши этой команды происходит считывание даных
+string username = Console.ReadLine(); // string(строка) - это тип даных; username - это имя переменой; = - оператор присваивание
+Console.WriteLine("Привет, ");        
+Console.WriteLine(username);
