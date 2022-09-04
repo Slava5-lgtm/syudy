b2e7d25c806f72b5ceb4803057ed62f88a21b678
Vaceslav <vaceslavsergunin@gmail.com>
2022-09-04 13:31:58 +0600

    удолил

--- Language/C#/lession-1/Exampels002_HeloUser/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿Console.WriteLine("Введите ваше имя"); // ReadLine - при помоши этой команды происходит считывание даных
-string username = Console.ReadLine(); // string(строка) - это тип даных; username - это имя переменой; = - оператор присваивание
-Console.WriteLine("Привет, ");        
-Console.WriteLine(username);
