69eb62e3cf8487c01b709a2b9cf8ac8f1f789012
Vaceslav <vaceslavsergunin@gmail.com>
2022-09-04 09:18:23 +0600

    написал програму

---
+++ Language/C#/lession-1/Exampels003_Сложения_двух_чисел/Program.cs
@@ -0,0 +1,10 @@
+﻿//int numberA = 3;
+//int numberB = 5;
+//Console. WriteLine(numberA + numberB);
+
+// можно делать и через дополнительную переменую
+
+int numberA = 31;
+int numberB = 5;
+int result  = numberA + numberB;
+Console.WriteLine(result);
