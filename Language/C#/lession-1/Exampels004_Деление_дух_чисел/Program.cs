e636471a09eae25f3895587cfab3f647344464af
Vaceslav <vaceslavsergunin@gmail.com>
2022-09-04 09:35:50 +0600

    написал програму

---
+++ Language/C#/lession-1/Exampels004_Деление_дух_чисел/Program.cs
@@ -0,0 +1,10 @@
+﻿double numberA = 31;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
+
+
+// типы даных
+// int - целое число
+// double - вешествение числа
+// string - строки
+// bool (true и false) - логический тип даных истина или ложь
