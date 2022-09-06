64ff738057181f2cc27f85fea35d6d1ff1906dad
Vaceslav <vaceslavsergunin@gmail.com>
2022-09-06 21:31:21 +0600

    добавил програму

--- Language/C#/lession-1/Exampels003.1_Сложене_двух_случайных_чисел/Program.cs
+++ Language/C#/lession-1/Exampels003.1_Сложене_двух_случайных_чисел/Program.cs
@@ -1,8 +1,7 @@
 ﻿int numberA = new Random().Next(1, 10); // 1 2 3 4 .... 9
 Console.WriteLine(numberA); // показывает цифры в терменале
 int numberB = new Random().Next(1, 10); // 1 2 3 4 .... 9
-Console.WriteLine(numberA); // показывает цифры в терменале
+Console.WriteLine(numberB); // показывает цифры в терменале
 int result = numberA + numberB;
-Console. WriteLine(numberA + numberB);
 Console.WriteLine(result);
 
