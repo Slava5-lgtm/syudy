08605bfa2c85887e25dda4a2ab04ef5fd1dcc665
Vaceslav <vaceslavsergunin@gmail.com>
2022-09-18 23:55:19 +0600

    добавил програму

---
+++ Language/C#/Seminar_1/DZ/Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine ("Введите любо целое положительное число");
+
+string number = Console.ReadLine();
+
+int number1 = int.Parse (number);
+
+int n = 1;
+
+bool not = true;
+
+Console.WriteLine("Чётные числа от 1 до " + number1);
+            while (n <= number1)
+            {
+                if (n % 2 != 1)
+                {
+                    Console.Write(n + ", ");
+                    not = false;
+                }
+                n++;
+            }
