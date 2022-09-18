8a0a25be8d04531001988a19aadbfdd883f10845
Vaceslav <vaceslavsergunin@gmail.com>
2022-09-18 22:45:03 +0600

    изменил текст

--- Language/C#/Seminar_1/DZ/Task1/Program.cs
+++ Language/C#/Seminar_1/DZ/Task1/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите целое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if(numberB > max) max = numberB;
+if(numberC > max) max = numberC;
+Console.WriteLine(max);
