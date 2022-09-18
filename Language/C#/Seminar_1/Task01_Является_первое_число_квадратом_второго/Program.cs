62ba696be0f55bd656abd81440d96a3d2ab65fe7
Vaceslav <vaceslavsergunin@gmail.com>
2022-09-18 21:29:13 +0600

    изменил програму

--- Language/C#/Seminar_1/Task01_Является_первое_число_квадратом_второго/Program.cs
+++ Language/C#/Seminar_1/Task01_Является_первое_число_квадратом_второго/Program.cs
@@ -5,12 +5,12 @@ Console.Write("Ввдите первое число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Ввдите второе число: ");
 int b = Convert.ToInt32(Console.ReadLine());
-int sqr_b = b * b;
-if (sqr_b == a)
+int sqrb = b * b;
+if (sqrb == a)
 {
     Console.Write($"Да, первое число {a} - это квадрат второго числа {b}");
 }
 else
 {
-    Console.Write($"Нет, первое число {a} - это не квадрат второго чила {b}. Квадрат числа {b} - это {sqr_b}");
+    Console.Write($"Нет, первое число {a} - это не квадрат второго чила {b}. Квадрат числа {b} - это {sqrb}");
 }
