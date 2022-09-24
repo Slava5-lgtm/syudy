ff9eeb846738b00b7261ad412c8a1891b3a190db
Vaceslav <vaceslavsergunin@gmail.com>
2022-09-24 11:54:38 +0600

    добавил

---
+++ Language/C#/lesion-2_Методы_и_массивы/Seminar_2/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 9. Напишите программу, которая выводит
+// 1. случайное число из отрезка [10, 99] и
+// 2. показывает наибольшую цифру числа.
+// 3. Вывод результата
+
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+int number = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из отрезка 10 - 99 => {number}");
+
+int MaxDigit (int num)
+{
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+    if(firstDigit == secondDigit) return -1;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
+
+bool isEqualDigits(int num1, int num2)
+{
+    return num1 == num2;
+}
+
+
+int maxDigit = MaxDigit(number);
+string result = maxDigit != -1 ? maxDigit.ToString(): "цифры одинаковы";
+Console.WriteLine($"Наибольшое цифра числа {number} => {result}");
+
--- Language/C#/lesion-2_Методы_и_массивы/Seminar_2/Task_3/Program.cs
+++ Language/C#/lesion-2_Методы_и_массивы/Seminar_2/Task_3/Program.cs
@@ -8,19 +8,19 @@
 // 85 -> 8
 int number = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число из отрезка 10 - 99 => {number}"); // без этой строки если цифры равны не будет показывать слуйчайное число
-int firstDigit = number / 10;
-int secondDigit = number % 10;
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
 
-if (firstDigit == secondDigit) Console.WriteLine("Цифры одинаковые"); // == - равен, != - не равен
-else if(firstDigit > secondDigit) Console.WriteLine($"Наибольшое цифра числа {number} => {firstDigit}");
-else Console.WriteLine($"Наибольшое цифра числа {number} => {secondDigit}");
+// if (firstDigit == secondDigit) Console.WriteLine("Цифры одинаковые"); // == - равен, != - не равен
+// else if(firstDigit > secondDigit) Console.WriteLine($"Наибольшое цифра числа {number} => {firstDigit}");
+// else Console.WriteLine($"Наибольшое цифра числа {number} => {secondDigit}");
 
-int maxDigit = 0;
-if (firstDigit > secondDigit) maxDigit = firstDigit;
-else maxDigit = secondDigit;
+// int maxDigit = 0;
+// if (firstDigit > secondDigit) maxDigit = firstDigit;
+// else maxDigit = secondDigit;
 // Тернадный оператор
 // если firstDigit больше secondDigit тогда мы в переменую max записываем firstDigit иначе secondDigit
-int max = firstDigit > secondDigit ? firstDigit : secondDigit;
+// int max = firstDigit > secondDigit ? firstDigit : secondDigit;
 
 
 int MaxDigit (int num)
@@ -30,3 +30,6 @@ int MaxDigit (int num)
     if (firstDigit > secondDigit) return firstDigit;
     else return secondDigit;
 }
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшое цифра числа {number} => {maxDigit}");
