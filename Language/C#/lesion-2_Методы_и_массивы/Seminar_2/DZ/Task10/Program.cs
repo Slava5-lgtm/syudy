a6ea1c69777da7f58fc0dd3cb182aec0726a2605
Vaceslav <vaceslavsergunin@gmail.com>
2022-09-25 01:33:24 +0600

    добавил

---
+++ Language/C#/lesion-2_Методы_и_массивы/Seminar_2/DZ/Task10/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число из отрезка: {number}");
+// метод
+int Doub(int num)
+{
+    
+  
+    int third = number / 100;
+    int second = number / 10  % 10;
+   return second;
+}
+int result = Doub(number);
+Console.WriteLine($"Искомое число {result}");
+
+
