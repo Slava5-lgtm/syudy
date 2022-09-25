c1aee25d3c5eded72f428d2ec81034a0cf266b88
Vaceslav <vaceslavsergunin@gmail.com>
2022-09-25 15:36:56 +0600

    добавил

---
+++ Language/C#/lesion-3/Exampels3/Program.cs
@@ -0,0 +1,20 @@
+﻿// МЕТОД 3 - Что ворощяют но нечего не возрошяют
+
+// int Method3()                   - имя метода
+// {
+//     return DateTime.Now.Year;   - оператор return текущая дата
+// }
+
+// int year = Method3();           - вызов метода указываем имя метода, вводим переменую (year)
+//                                 - через оператор присваевания = кладем нужное значения
+// Console.WriteLine(year);        - указываем имя переменой  иполучаем то значения который вернул метод
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);
+
+
+
---
+++ Language/C#/lesion-3/Exampels4/Program.cs
@@ -0,0 +1,32 @@
+﻿// МЕТОД Что возрошяют, что то принемают
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+
+
+string res = Method4(10, "12");
+Console.WriteLine(res);
+// string Method4(int count, string text) - строку возврошяет мы будем строку передовать count раз
+//                             Цикл
+// {
+//     int i = 0;
+//     string result = String.Empty;      - переменая (result) куда кладем результат = пустая трока (String.Empty)
+//     while (i < count)                  -  
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;                     -
+// }
+
+
+// string res = Method4(10, "12");        - вызов метода Method4 (сколько раз выводиться (10), что выводиться(12))
+// Console.WriteLine(res);
