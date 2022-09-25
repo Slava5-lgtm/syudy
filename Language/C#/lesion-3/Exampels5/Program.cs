57e0b5325740cffa2b4600d5b622abfd22721497
Vaceslav <vaceslavsergunin@gmail.com>
2022-09-25 16:59:06 +0600

    добавил

---
+++ Language/C#/lesion-3/Exampels5/Program.cs
@@ -0,0 +1,29 @@
+﻿// for в отличии от while все в одной строке 
+
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+
+string Method4(int count, string text)  
+{
+    
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+    
+
+}
+string res = Method4(10, "12");
+Console.WriteLine(res);
+
