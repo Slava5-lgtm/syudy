e63720dd2f5540078549230cc96ec8035e372d1a
Vaceslav <vaceslavsergunin@gmail.com>
2022-09-27 20:22:45 +0600

    добавил

---
+++ Language/C#/Seminar-3/Task18/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите номер четверти");
+string quarterNum = Console.ReadLine();
+
+string FindRange(string num)
+{
+    string phrase = "Деапозон координат находиться в пределах";
+    if (num == "1") return "Деапозон координат в пределах x > 0 и y > 0";
+    if (num == "2") return "Деапозон координат в пределах x < 0 и y > 0";
+    if (num == "3") return "Деапозон координат в пределах x < 0 и y < 0";
+    if (num == "4") return "Деапозон координат в пределах x > 0 и y < 0";
+    return "Введены не верные данные";
+}
+
+
+string range = FindRange(quarterNum);
+Console.WriteLine(range);
---
+++ Language/C#/lesion-2_Методы_и_массивы/Seminar_2/1/Program.cs
@@ -0,0 +1,5 @@
+﻿int number = ReadInt("Введите число: ");
+int count = number.ToString().Length;
+int[] number = {number};
+void PrintArray(int[] number)
+Console.ReadLine(number[2]);
