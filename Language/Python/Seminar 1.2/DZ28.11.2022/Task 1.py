a884b50ac7c254e7d9ab0e38e9f3ebf06f5c1b10
Vaceslav <vaceslavsergunin@gmail.com>
2022-12-01 23:28:30 +0600

    изменил

--- Language/Python/Seminar 1.2/DZ28.11.2022/Task 1.py
+++ Language/Python/Seminar 1.2/DZ28.11.2022/Task 1.py
@@ -5,11 +5,12 @@
 # 0,56 -> 11
 
 n = float(input("Введите число:"))
-tot = 0
-while (n > 0):
-    dig = n % 10
-    tot = tot + dig
-    n = n//10
-print("Сумма цифр равна:", tot)
+# tot = 0
+# while (n > 0):
+i = 1
+while i <= n:
+    print((3+i), end=', ')
+    i += 1
+print("Сумма цифр равна:", i)
 
 
