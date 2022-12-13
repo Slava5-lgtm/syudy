74ce85f6ffd05dbc295815f026b3da398bb60c50
Vaceslav <vaceslavsergunin@gmail.com>
2022-12-14 05:36:05 +0600

    добавил

--- Language/Python/Lesson-4/Task-1/main.py
+++ Language/Python/Lesson-4/Task-1/main.py
@@ -1,4 +1,5 @@
-# Нам потребуеться для начало импортировать наши модули
+# encoding = utf - 8 Нам потребуеться для начало импортировать наши модули
+import csv
 import html_creater as hc
 import xml_generator as xg
 import data_provider as dp
