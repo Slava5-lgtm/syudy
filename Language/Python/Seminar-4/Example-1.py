3a9ed18c63103fc27ee32d06982f4ee937da66ea
Vaceslav <vaceslavsergunin@gmail.com>
2022-12-16 18:59:46 +0600

    добавил

--- Language/Python/Seminar-4/Example-1.py
+++ Language/Python/Seminar-4/Example-1.py
@@ -9,7 +9,7 @@
 
 # from pathlib import Path                                # Это специальная библеотека для работы с путями
 
-# file_path = Path('data', 'tst.txt')                     # Создаем путь к нашей папке
+# file_path = Path('data', 'test.txt')                     # Создаем путь к нашей папке
 # with open(file_path, 'r') as f_data:                    # Откроем в режиме чтения
 #     print(f_data.read())#
 
@@ -18,6 +18,13 @@
 # В качестве символа-разделителя используйте пробел. Результат запишите в исодный файл(minn maxx).
 
 
+f_path = 'test.txt'
 
-with open('Task1.txt', 'r') as f_data:
-    print(f_data.read())
+with open(f_path, 'r') as f_nums:
+    list_nums = f_nums.read().split(' ')
+for i in range(len(list_nums)):
+    list_nums[i] = int(list_nums[i])
+
+minmax_list = [min(list_nums), max(list_nums)]
+with open(f_path, 'a') as min_max:
+    min_max.writelines(f"\n {minmax_list} ")
